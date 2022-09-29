29eba61992f4931c774de8696a8466421abefe92
Thiago Henrique De Lima Jaques <thiagohenriquedelimajaques@gmail.com>
2022-09-29 17:02:09 -0300

    atualization

--- exe022.py
+++ exe022.py
@@ -1,7 +1,10 @@
-frase = str(input('Digite o seu nome completo?: '))
-print('Assim fica com todas as letra maiúsculas:')
-print(frase.upper())
-print('Com todas minúsculas: ')
-print(frase.lower())
-nomediv = frase.split()
-print(len(nomediv))
+from itertools import count
+
+
+frase = str(input('Digite o seu nome completo?: ')).strip()
+print('Analisando seu nome...')
+print('Assim fica com todas as letra maiúsculas: {}'.format(frase.upper()))
+print('Com todas minúsculas: {}'.format(frase.lower()))
+print('{} tem {} letras.'.format(frase, len(frase)- frase.count(' ')))
+separa = frase.split()
+print('Seu primeiro nome é {}, e tem {} letras.'.format(separa[0], len(separa[0])))
--- exe023.py
+++ exe023.py
@@ -1,3 +1,9 @@
+from ast import Num
 import random
-n1 = random.randint(0,9999)
-print(n1)
+n1 = int(input('Informe um número de 0 á 999: '))
+print('Analisando o número: {}'.format(n1))
+n = str(n1)
+n.split()
+print('Centena: {}'.format(n[0]))
+print('Dezena: {}'.format(n[1]))
+print('Milhar: {}'.format(n[2]))
